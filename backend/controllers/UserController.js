ea92c111bdf3adddb9c92681945de9adf1fb31d8
James Li-Xu <36280940+james-lixu@users.noreply.github.com>
2024-09-29 13:42:07 +1000

    added controller folder
    usercontroller added

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+
+//Register
+const userRegistration = async (req, res) => {
+  const { username, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(400).json({ error: "Email is already in use" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Error registering user" });
+  }
+};
+
+//Login
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
+
+    // If the user exists and the password is correct
+    res.status(200).json({ message: "Login successful", user: user.username });
+  } catch (err) {
+    res.status(500).json({ error: "Error logging in" });
+  }
+};
+
+module.exports = {
+    userRegistration,
+    userLogin,
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,47 +1,11 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
 const express = require("express");
+const { userRegistration, userLogin } = require('../controllers/UserController');
 const router = express.Router();
-const User = require('../models/User')
 
 // User Registration
-router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({ error: 'Email is already in use' });
-  }
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    await newUser.save();
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (err) {
-    res.status(500).json({ error: "Error registering user" });
-  }
-});
+router.post("/register", userRegistration) 
 
 // User Login
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ error: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ error: "Error logging in" });
-  }
-});
+router.post("/login", userLogin);
 
 module.exports = router;
